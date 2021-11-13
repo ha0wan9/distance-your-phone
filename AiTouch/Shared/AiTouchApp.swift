2ae005defff8e2a4aef7a3fdf67ef690ea10cf1c
ha0wan9 <haoran.wang58@gmail.com>
2021-11-13 01:30:56 +0100

    add swift project. [iOS App]

---
+++ AiTouch/Shared/AiTouchApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiTouchApp.swift
+//  Shared
+//
+//  Created by Haoran WANG on 13/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct AiTouchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
